bddf7a6a734da5b767feec12dca1377b52aebc47
Devanshu <devanshumishra246@gmail.com>
2026-03-20 19:09:49 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import JobGuard from '@/components/JobGuard';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-slate-50 text-slate-900 font-sans py-12">
+      <JobGuard />
+    </main>
+  );
+}
